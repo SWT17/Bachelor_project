d25a36f35fd1e72e259c45c219c9ad14d6534754
Nicolaj Kvejborg Hansen <au461460@uni.au.dk>
2021-12-05 13:04:20 +0100

    changes done to the second iteration code

--- Full system/Microcontroller_coolingsystem.cydsn/Regulation.c
+++ Full system/Microcontroller_coolingsystem.cydsn/Regulation.c
@@ -17,21 +17,21 @@ float upper_limit = -9;
 uint8 res;
 
 
-void Regulation_setup()
-{
-    res = 1;
-}
-
-uint8 Regulation(float temp)
-{
-    if(temp > upper_limit)
-    {
-        res = 1;
-        
-    }else if( temp < lower_limit)
-    {
-        res = 0;
-    }
-    return res;
-}
+//void Regulation_setup()
+//{
+//    res = 1;
+//}
+//
+//uint8 Regulation(float temp)
+//{
+//    if(temp > upper_limit)
+//    {
+//        res = 1;
+//        
+//    }else if( temp < lower_limit)
+//    {
+//        res = 0;
+//    }
+//    return res;
+//}
 /* [] END OF FILE */
